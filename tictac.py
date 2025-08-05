a6d7fe586d08879fe7d15c489bd148258bc83d0a
yanis26x <yanis.djenadi@hotmail.com>
2025-08-05 18:17:42 -0400

    son hehe qui se met a chaque x la fameeee

--- tictac.py
+++ tictac.py
@@ -19,7 +19,6 @@ son_victoire = pygame.mixer.Sound(get_path("OST/yay.wav"))
 son_dahak = pygame.mixer.Sound(get_path("OST/BASKETpELE.wav"))
 son_woo = pygame.mixer.Sound(get_path("OST/woo.wav"))
 
-
 # Création de la fenêtre
 T = 500
 screen = pygame.display.set_mode((T, T))
@@ -59,24 +58,28 @@ def dessiner():
         text_btn = font.render("Recommencer", True, (0, 0, 0))
         screen.blit(text_btn, (T//4 + 20, T//2 + 10))
     elif all(all(cell is not None for cell in row) for row in grille):
-
         text = font.render("TES UN GROS SAC DE PATATES, TES POURIE !", True, (255, 0, 0))
         screen.blit(text, (T//6, T//3))
         pygame.draw.rect(screen, (255, 0, 0), (T//4, T//2, T//2, 50))
         text_btn = font.render("Recommencer", True, (0, 0, 0))
         screen.blit(text_btn, (T//4 + 20, T//2 + 10))
     
-    # Afficher "yanis26x" en bas à droite
+    # Signature en bas à droite
     font_signature = pygame.font.Font(None, 30)
-    signature = font_signature.render("yanis26x", True, (255, 0, 255))  #@yanis27x A METTRE !!!!!!!!!!!!!!!!
+    signature = font_signature.render("yanis26x", True, (255, 0, 255))
     screen.blit(signature, (T - 100, T - 30))
     pygame.display.flip()
 
 # Vérifier la victoire
 def victoire(j):
-    return any(all(grille[y][x] == j for x in range(3)) for y in range(3)) or any(all(grille[y][x] == j for y in range(3)) for x in range(3)) or all(grille[i][i] == j for i in range(3)) or all(grille[i][2-i] == j for i in range(3))
+    return any(all(grille[y][x] == j for x in range(3)) for y in range(3)) or \
+           any(all(grille[y][x] == j for y in range(3)) for x in range(3)) or \
+           all(grille[i][i] == j for i in range(3)) or \
+           all(grille[i][2 - i] == j for i in range(3))
 
-show_start_screen(screen, T) # ecrand start
+# Afficher l'écran de démarrage une première fois
+show_start_screen(screen, T)
+
 # Boucle du jeu
 running = True
 while running:
@@ -84,19 +87,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos[0] // (T//3), event.pos[1] // (T//3)
+            x, y = event.pos[0] // (T // 3), event.pos[1] // (T // 3)
             if gagnant or all(all(cell is not None for cell in row) for row in grille):
                 if T//4 < event.pos[0] < T//4 + T//2 and T//2 < event.pos[1] < T//2 + 50:
                     grille = [[None]*3 for _ in range(3)]
                     gagnant = None
+                    show_start_screen(screen, T)  # 👉 Réaffiche l'écran de démarrage
             elif not grille[y][x]:
                 grille[y][x] = joueur
                 if victoire(joueur):
                     gagnant = f"Joueur {'1' if joueur == 'X' else '2'}"
                     pygame.mixer.Sound.play(son_victoire)
                 joueur = 'O' if joueur == 'X' else 'X'
-                pygame.mixer.Sound.play(son_woo) #avant c t -> son_dahak
+                pygame.mixer.Sound.play(son_woo)
 
 pygame.quit()
 sys.exit()
